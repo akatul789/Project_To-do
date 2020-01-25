656791c2525f66ccd3e018c501d33b6f16abd241
akatul789 <akatul789@gmail.com>
2020-01-25 12:49:11 -0500

    Test js

---
+++ test.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const Sequelize = require('sequelize');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+const port = 8000;
+
+const sequelize = new Sequelize({
+	username: "aks1",
+    password: 'aks101',
+	database: "postgres",
+    host: "localhost",
+    dialect: "postgres"
+});
+
+///-------------
+
+sequelize.authenticate()
+.then( ()=> {
+    console.log("Database Connected");
+}).catch( err => {
+    console.error('unable to connect to Database');
+});
+
+
+///// ---------
+const User = sequelize.define('user',{
+
+    user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: Sequelize.STRING,
+
+    email: {
+        type: Sequelize.STRING,
+        unique: true,
+            validate: {
+                isEmail: true
+            }
+            },
+    prof_pic: Sequelize.STRING
+    
+    },{
+        timestamps: false
+});
+//---------------------
+const to_do = sequelize.define('to_do',
+{
+
+    id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    title: Sequelize.STRING,
+    description: Sequelize.STRING,
+    user_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+       references: {
+          model: 'users',
+          key: 'user_id', 
+       }
+    },
+    
+    
+    },{
+        timestamps: false
+});
+
+User.hasMany(to_do); /// one to many rel.
+
+//---------------------
+
+const Login = sequelize.define('login',
+{
+
+    log_id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+	password: Sequelize.STRING,
+	email: {
+        type: Sequelize.STRING,
+    	//allowNull: false,
+        references: {
+          model: 'users',
+          key: 'email', 
+       }
+    },
+    
+    
+    },{
+        timestamps: false
+});
+
+//User.belongsTo(Login);
+
+//------------
+ sequelize.sync({
+     force: true
+ });
