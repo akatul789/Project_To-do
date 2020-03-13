63090b4589f5a38fba62d435d9042a96f8b1da4b
Navaneeth Nivol <navaneethsai1999@gmail.com>
2020-03-13 11:29:22 +0530

    server config

--- README.md
+++
@@ -1,2 +0,0 @@
-# Test_proj_To-do
-Project on To-do app(back-end) in nodejs
--- test.js
+++ test.js
@@ -10,12 +10,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-const port = 8000;
+const port = 3100;
 
 const sequelize = new Sequelize({
-	username: "aks1",
-    password: 'aks101',
-	database: "postgres",
+	username: "postgres",
+    password: 'Admin@123',
+	database: "todo",
     host: "localhost",
     dialect: "postgres"
 });
@@ -184,30 +184,36 @@ app.post('/create/user',authHandler, async (req,res) => {
     }
 
    try{
+
+        const t1 = await sequelize.transaction();
        
-        var user_created = await User.create(newUser);
+        var user_created = await User.create(newUser, { transaction: t1 });
         
-        var login=await Login.create(logindet);
+        var login=await Login.create(logindet, { transaction: t1 });
 		
         console.log(user_created);
         
         if(user_created)
         {
+            await t1.commit();
             res.status(200).json({
                 message: "User created successfully !!"
             });
             return;
         }
+        else{
+            await t1.rollback();
+        }
    }
    catch(err){
     console.log(err);
+    await t1.rollback();
     res.status(500).json({
         message: "User not created !!",
         error: err
     });
     return;
    }
-
 })
 
 //---------------------- Create TO-do api --------------------
